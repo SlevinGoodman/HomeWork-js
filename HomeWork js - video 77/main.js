832c1d0e210c2724d91079ff75c882c70e465622
SlevinGoodman <127791310+SlevinGoodman@users.noreply.github.com>
2023-08-17 09:21:00 +0300

    Add files via upload

---
+++ HomeWork js - video 77/main.js
@@ -0,0 +1,14 @@
+const num1 = 7
+const num2 = 10
+const num3 = 15
+const num4 = 25
+const num5 = 59
+const num6 = 99
+
+console.log(num6%8)
+console.log(num4**5)
+console.log((num5+num1)/num2)
+console.log(num3===num1)
+console.log(num4!==num1)
+console.log(num1>num2)
+console.log('good ' + 'morning')
