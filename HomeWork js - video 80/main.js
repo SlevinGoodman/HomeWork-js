60934f6be5b5801bea64adc652a56699c7ba2309
SlevinGoodman <127791310+SlevinGoodman@users.noreply.github.com>
2023-08-17 21:03:59 +0300

    Add files via upload

---
+++ HomeWork js - video 80/main.js
@@ -0,0 +1,20 @@
+
+for (let i = 2; i <= 12; i = i + 2) {
+    console.log (`first task with for ${i}`)
+}
+
+for(let a = 10; a >= 1; a = a - 1) {
+    console.log(`second task with for ${a}`)
+}
+
+let u = 2
+while(u <= 12) {
+    console.log(`first cycle with while ${u}`)
+u = u + 2
+}
+
+let y = 10
+while(y >= 1) {
+    console.log(`second cycle with while ${y}`)
+    y = y - 1
+}
