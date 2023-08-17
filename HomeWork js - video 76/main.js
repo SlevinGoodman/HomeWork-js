0e141079e3edd505399592655ef41f38d7a78834
SlevinGoodman <127791310+SlevinGoodman@users.noreply.github.com>
2023-08-17 09:20:38 +0300

    Add files via upload

---
+++ HomeWork js - video 76/main.js
@@ -0,0 +1,7 @@
+const firstName = 'Slevin'
+const lastName = 'Goodman'
+let age = 30
+let country = 'Russia'
+const tall = 182
+
+console.log(firstName, lastName, age, 'y.o.', 'from', country, tall, 'cm. tall' )
