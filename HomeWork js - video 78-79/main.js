2e7f32e45d2944af64d5e48b6bb4bd31d018643a
SlevinGoodman <127791310+SlevinGoodman@users.noreply.github.com>
2023-08-17 21:03:41 +0300

    Add files via upload

---
+++ HomeWork js - video 78-79/main.js
@@ -0,0 +1,14 @@
+let age = 30
+
+if (age >= 18) { 
+    console.log('Вы совершеннолетний')
+} else {
+    console.log('Вам еще нет 18')
+}
+
+age >= 18 ? console.log('Вы совершеннолетний') : console.log('Вам еще нет 18')
+
+
+
+
+
