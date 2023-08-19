206bd8a2018a8699c87709196f978bb172cdf7e7
SlevinGoodman <127791310+SlevinGoodman@users.noreply.github.com>
2023-08-19 18:35:41 +0300

    Add files via upload

---
+++ HomeWork js - video 81/main.js
@@ -0,0 +1,14 @@
+// HW task #1
+const numbers = [2, 11, 22, 33, 44, 55, 67, 78, 89, 100] 
+for (let i = 0; i < numbers.length; i++) {
+    if ((numbers[i] % 2) === 0)
+    {
+        console.log(numbers[i])
+    }
+    }
+
+// HW task #2
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+for (let y = 5; y < colors.length; y = y - 1) {
+    console.log(colors[y])
+}
